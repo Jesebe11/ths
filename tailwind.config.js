52143375c34e33f4dd98c5ca3fb0bbd32e7357b7
jescobar <jesebellinformatica@gmail.com>
2021-09-23 15:10:12 -0430

    Commit initial

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        //layout
+        'nezegamer-red' : '#df163b',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
