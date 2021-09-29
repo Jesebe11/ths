67f1b12f21e0d8831a80384b07043e8a22bdea69
jescobar <jesebellinformatica@gmail.com>
2021-09-29 17:09:24 -0430

    Was added colours palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       colors:{
         //layout
-        'ths-black' : '#000',
+        'global-black' : '#080A0C',
+        'global-white' : '#FFF',
+        'global-blue-primary' : '#15C7E7',
+        'global-blue-secondary' : '#32DFFE',
+        'global-gray-light' : '#7A7A7A',
       }
     },
   },
