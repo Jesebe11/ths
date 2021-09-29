3b272976b42d0d42a5712a289e02671d0f75b5fa
jescobar <jesebellinformatica@gmail.com>
2021-09-29 17:12:11 -0430

    was added a second gray color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         'global-white' : '#FFF',
         'global-blue-primary' : '#15C7E7',
         'global-blue-secondary' : '#32DFFE',
-        'global-gray-light' : '#7A7A7A',
+        'global-gray-primary' : '#1A1E23',
+        'global-gray-secondary' : '#7A7A7A',
       }
     },
   },
