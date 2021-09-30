fba164514a9dee59b9ecf654b83dd60e1fa3df37
jescobar <jesebellinformatica@gmail.com>
2021-09-30 12:13:39 -0430

    Was been added a font family "Roboto Slab" to tailwind file config .js

--- home.php
+++ home.php
@@ -1,59 +1,13 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package ths_theme
- */
-
-get_header();
+	get_header();
 ?>
-<h1 class="titulo">Holaaaaaaaaaaa</h1>
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path></svg>
+
+<main id="primary" class="site-main">
+	<h1 class="titulo">Holaaaaaaaaaaa wuapo, estas perdio... soy el home</h1>
 
 
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
+	</main>
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
+      fontFamily: {
+        'roboto-slab': ['"Roboto Slab"']
+      },
       colors:{
-        //layout
         'global-black' : '#080A0C',
-        'global-white' : '#FFF',
+        'global-gray-primary' : '#1A1E23',
         'global-blue-primary' : '#15C7E7',
         'global-blue-secondary' : '#32DFFE',
-        'global-gray-primary' : '#1A1E23',
+        'global-white' : '#FFF',
         'global-gray-secondary' : '#7A7A7A',
       }
     },
