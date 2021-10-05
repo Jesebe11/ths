ee308808677a368e403b2ac203184d2aa39e3c12
jescobar <jesebellinformatica@gmail.com>
2021-10-05 17:24:05 -0430

    Was added styles for newsletter section and form

--- template-parts/common/footer/newsletter.php
+++ template-parts/common/footer/newsletter.php
@@ -1,3 +1,21 @@
 <div class="newsletter">
-    aqui va el newsletter
+    <div class="newsletter__all">
+        <div class="info">
+            <h1 class="info__title">
+                ¡Suscríbete a nuestro Newsletter!
+            </h1>
+            <p class="info__action">
+                A través de nuestro newsletter y boletín de noticias
+                le mantendremos informado a diario de todas
+                nuestras novedades y las noticias más importantes
+                del mundo tech.
+            </p>
+        </div>
+        <form action="" class="form-newsletter">
+            <input type="email" class="form-newsletter__email" placeholder="Introduce tu email">
+            <button type="submit" class="form-newsletter__send">
+                <span>Enviar</span>
+            </button>
+        </form>
+    </div>
 </div>
