ec74cdaed1709bc43ec34ea5b56baaed8d427c51
jescobar <jesebellinformatica@gmail.com>
2022-08-22 15:30:05 -0430

    Was changed a slider code simple for a plugin very cute

--- template-parts/components/content/home/bh-slider.php
+++ template-parts/components/content/home/bh-slider.php
@@ -1,30 +1,3 @@
-<div class="slider">
-    <div class="slider__content" style="height: 40vh">
-        <div class="slider__content__item" id="slide-1">
-            <img class="image" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80">
-            <h1 class="z-20 text-center">Any kind of content here, images too!</h1>
-        </div>
-
-        <div class="slider__content__item" id="slide-2">
-            <img class="image" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80">
-            <h1 class="z-20 text-center">Any kind of content here, images too!</h1>
-        </div>
-
-        <div class="slider__content__item" id="slide-3">
-            <img class="image" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80">
-            <h1 class="z-20 text-center">Any kind of content here, images too!</h1>
-        </div>
-
-        <div class="slider__content__item" id="slide-4">
-            <img class="image" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80">
-            <h1 class="z-20 text-center">Any kind of content here, images too!</h1>
-        </div>
-
-    </div>
-    <div class="controls">
-        <a class="controls__item" href="#slide-1">1</a>
-        <a class="controls__item" href="#slide-2">2</a>
-        <a class="controls__item" href="#slide-3">3</a>
-        <a class="controls__item" href="#slide-4">4</a>
-    </div>
-</div>
+<?php
+    echo do_shortcode('[metaslider id="754"]');
+?>
