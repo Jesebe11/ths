a4ece05e14b2f31325c2d2fd58cf8689a135d6c2
jescobar <jesebellinformatica@gmail.com>
2022-08-22 17:43:40 -0430

    Was added styles for bh-our services

--- template-parts/components/content/home/bh-our-services.php
+++ template-parts/components/content/home/bh-our-services.php
@@ -4,7 +4,7 @@ $args = [
     'post_type' => 'post',
     'post_status' => 'publish',
     'orderby' => 'date',
-    'order' => 'DESC',
+    'order' => 'ASC',
     'posts_per_page' => 6,
     'category_name' => 'servicios'
 ];
@@ -12,24 +12,23 @@ $args = [
 $the_query = new WP_Query( $args);
 
 ?>
-<section class="services">
-
     <?php
         get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Nuestros</span>&nbsp servicios' , 'permalink' => '  '] );
     ?>
+<section class="services">
+
         <?php if( $the_query->have_posts() ) : ?>
             <?php while( $the_query->have_posts() ) :
                 $the_query->the_post();
                 $title = get_the_title();
 
             ?>
-                <article class="content">
+                <article class="card">
+
+                    <h2 class="content-title"><?= $title; ?></h2>
 
                     <?= get_the_post_thumbnail( $post->ID, 'medium', array('class' => 'content-image') ); ?>
-                    <a class="zoom-gallery" href="<?php the_permalink(); ?>">
-                        <h2 class="content-title"><?= $title; ?></h2>
-                    </a>
-
+                    <a class="zoom-gallery" href="<?php the_permalink(); ?>"></a>
                             <div class="content-text">
                                 <?php  echo '<p class="excerpt">' . get_the_excerpt() . '</p>'; ?>
                             </div>
